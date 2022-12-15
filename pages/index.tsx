16ed7ad3e55f017c2f732ca58461d062bdfbc473
Milton Hernandez <cariasmilton2002@gmal.com>
2022-12-15 16:42:15 -0600

    add import the components

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,17 @@ import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
+import PageLayout from '../components/pegasLayout.jsx'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <PageLayout>
       <div className={styles.container}>
         <h1>Aprendiendo con Next.js desde cero</h1>
         <Link href='/about'> Ir a about</Link>
       </div>
-
+      </PageLayout>
   )
 }
